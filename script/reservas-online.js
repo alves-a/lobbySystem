b1b4e04d02100ab95de1250ddfd18b42935c397b
Afonso Alves <alves.afonso@users.noreply.github.com>
2020-10-24 16:35:41 -0300

    banco de dados

--- script/reservas-online.js
+++ script/reservas-online.js
@@ -1,3 +1,26 @@
-/* script que busca no banco de dados os hóspedes que fizeram reserva online */
+/* script que busca no banco de dados todos hóspedes que fizeram reserva online e adiciona na tabela */
 
 var botaoBuscar = document.querySelector("#cadastros-online");
+botaoBuscar.addEventListener("click", function () {
+   
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://next.json-generator.com/api/json/get/EkM36IpwY?indent=2");
+
+    xhr.addEventListener("load", function () {
+        var erroAjax = document.querySelector("#erro-ajax");
+        
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            
+            var listaDeReservas = JSON.parse(xhr.responseText);
+
+            listaDeReservas.forEach(hospedes => {
+                adicionaHospedeNaTabela(hospedes);
+            });
+            
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+    });
+    xhr.send();
+});
